46c6c47529eae74f3c1f73dff5c492cc9f099467
Jonathan Perez <jonathanperezmota@gmail.com>
2018-08-21 16:09:52 -0400

    Initial commit

---
+++ src/main/java/com/jonathanperez/perspective/PerspectiveApplication.java
@@ -0,0 +1,12 @@
+package com.jonathanperez.perspective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerspectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerspectiveApplication.class, args);
+	}
+}
