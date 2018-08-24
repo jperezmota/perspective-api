b8cb1620aa567e22888ff9c8f696a5fa11689119
Jonathan Perez <jonathanperezmota@gmail.com>
2018-08-24 16:26:15 -0400

    Changed @NotNull to @NotBlank.

--- src/main/java/com/jonathanperez/perspective/entities/Author.java
+++ src/main/java/com/jonathanperez/perspective/entities/Author.java
@@ -1,19 +1,12 @@
 package com.jonathanperez.perspective.entities;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -33,7 +26,7 @@ public class Author extends Auditable<String> {
 	private long id;
 	
 	@Column(name = "name")
-	@NotNull
+	@NotBlank
 	private String name;
 
 	public Author() {
