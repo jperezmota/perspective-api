29d01fbb1224bf357e1c0fcdd1e2208421354c13
Jonathan Perez <jonathanperezmota@gmail.com>
2018-08-24 20:25:39 -0400

    Implementing bcrypt password and Spring Security database authentication.

--- src/main/java/com/jonathanperez/perspective/config/WebSecurityConfiguration.java
+++ src/main/java/com/jonathanperez/perspective/config/WebSecurityConfiguration.java
@@ -1,30 +1,28 @@
 package com.jonathanperez.perspective.config;
 
-import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private DataSource dataSource;
+	
    @Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("admin").password("admin").roles("ADMIN"))
-			.withUser(users.username("user").password("user").roles("USER"));
+	   auth.jdbcAuthentication()
+	    .dataSource(dataSource)
+	    .passwordEncoder(new BCryptPasswordEncoder());
 	}
   
 	   
